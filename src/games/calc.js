b1d2fae780b65a9505c64ae53bbe4aa4b6cf867c
AmiAxe <maxidrom.92@yandex.ru>
2022-12-24 21:23:09 +0500

    new add

--- src/games/calc.js
+++ src/games/calc.js
@@ -14,14 +14,17 @@ const calcExpression = (a, b, operator) => {
 };
 
 const newGame = () => {
+  const a = Math.round(Math.random() * 10);
+  const b = Math.round(Math.random() * 10);
+  const arrayOperators = ['+', '-', '*'];
+  const indexArrayOperators = Math.round(Math.random() * 2);
+  const operator = arrayOperators[indexArrayOperators];
+  const question;
+  let answer;
 
-  console.log('What is the result of the expression?');
+
   for (let i = 0; i < 3; i += 1) {
-    const a = Math.round(Math.random() * 10);
-    const b = Math.round(Math.random() * 10);
-    const arrayOperators = ['+', '-', '*'];
-    const indexArrayOperator = Math.round(Math.random() * 2);
-    const operator = arrayOperators[indexArrayOperator];
+    
 
     console.log(`Question: ${a} ${operator} ${b}`);
     const answ = readlineSync.question('Your answer: ');
