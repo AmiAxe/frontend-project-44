7fbd0cb8198c0df8ebbcee0bd0080e26f6202124
AmiAxe <maxidrom.92@yandex.ru>
2022-12-25 21:01:55 +0500

    adds game brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,29 @@
+import mainLogic from '../index.js';
+
+const mainTask = 'Find the greatest common divisor of given numbers.';
+
+const countGcd = (bigNumber, smallNumber) => {
+  for (let i = smallNumber; i > 0 ; i -= 1) {
+    if (bigNumber % i === 0 && smallNumber % i === 0) {
+      return i;
+    }
+  }
+};
+
+const newGame = () => {
+  const a = Math.round(Math.random() * 100);
+  const b = Math.round(Math.random() * 100);
+  const question = `${a} ${b}`;
+  let answer = 0;
+
+  if (a > b) {
+    answer = countGcd(a, b);
+  } else {
+    answer = countGcd(b, a);
+  }
+  return [question, answer];
+};
+
+export default () => {
+  mainLogic(mainTask, newGame);
+};
